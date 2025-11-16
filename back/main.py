a89d4284a47fb76fc8ffb851d6a7ee21a9c9de64
Consol0302 <dohan018018@gmail.com>
2025-11-17 01:40:59 +0900

    d
    dd

--- back/main.py
+++ back/main.py
@@ -2,11 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client, Client
 from pydantic import BaseModel
-import supabase 
-import os
-from mangum import Mangum
-
-handler = Mangum(app)
 
 app = FastAPI(
     title="Blog API",
@@ -14,9 +9,9 @@ app = FastAPI(
     version="1.0.0"
 )
 
-supabase: Client = create_client("https://oujekswpyehyoixvnodf.supabase.co/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91amVrc3dweWVoeW9peHZub2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMDQxNjYsImV4cCI6MjA3ODY4MDE2Nn0.ycBrPlJEc9s4XAbHngEwLeakPvvL4wVjL0QGU35LBgo")
+# 변수명만 바꾸면 됩니다
+supabase_client: Client = create_client("https://oujekswpyehyoixvnodf.supabase.co/", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91amVrc3dweWVoeW9peHZub2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMDQxNjYsImV4cCI6MjA3ODY4MDE2Nn0.ycBrPlJEc9s4XAbHngEwLeakPvvL4wVjL0QGU35LBgo")
 
-# CORS 설정 (React 프론트엔드와 통신)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -29,14 +24,10 @@ app.add_middleware(
 def root():
     return "no"
 
-
 @app.get('/list')
 def list():
-    response = (
-        supabase.table("post").select("*").execute()
-    )
+    response = supabase_client.table("post").select("*").execute()
     return response.data
-
 
 class Comment(BaseModel):
     target: int
@@ -44,21 +35,14 @@ class Comment(BaseModel):
 
 @app.post('/sendComment')
 def sendComment(data: Comment):
-    response = (
-        supabase.table("comment").insert({"target_id": data.target, "content": data.content}).execute()
-    )
+    response = supabase_client.table("comment").insert({"target_id": data.target, "content": data.content}).execute()
     return True
-
 
 class getPost(BaseModel):
     postId: int
 
 @app.post('/postData')
 def getPostData(data: getPost):
-    response = (
-        supabase.table("post").select("*").eq("id", data.postId).execute()
-    )
-    commentResponse = (
-        supabase.table("comment").select("*").eq("target_id", data.postId).execute()
-    )
+    response = supabase_client.table("post").select("*").eq("id", data.postId).execute()
+    commentResponse = supabase_client.table("comment").select("*").eq("target_id", data.postId).execute()
     return [response.data, commentResponse.data]
